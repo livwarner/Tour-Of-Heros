71661d036bac4d6a58a34ab9dc10d8a54a9b5782
owarner <owarner@ceiamerica.com>
2021-10-04 16:26:25 -0400

    initial project creation and material install

---
+++ angular-tour-of-heros/src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class MaterialModule { }
